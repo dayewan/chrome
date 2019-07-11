e1b94914fc00625455ea9f0e8b583e0d8930e59e
sense <sense@gmail.com>
2019-07-11 19:17:55 +0800

    change

--- js/content.js
+++ js/content.js
@@ -1,23 +1,16 @@
 function injectCustomJs(jsPath) {
-    jsPath = jsPath || 'js/inject.js';
-    var temp = document.createElement('script');
-    temp.setAttribute('type', 'text/javascript');
-    temp.src = chrome.extension.getURL(jsPath);
-    document.head.appendChild(temp);
+  jsPath = jsPath || "js/inject.js";
+  var temp = document.createElement("script");
+  temp.setAttribute("type", "text/javascript");
+  temp.src = chrome.extension.getURL(jsPath);
+  document.head.appendChild(temp);
 }
 
-window.onload = function () {
-    if (location.host === 'detail.1688.com') {
-        injectCustomJs();
-    }
-    if (/seller.*?waiwaimall.com/.test(location.host)) {
-        injectCustomJs();
-    }
-    if (/admin.*?cucoe.com/.test(location.host)) {
-        injectCustomJs();
-    }
-    if (/admin.*?waiwaimall.com/.test(location.host)) {
-        injectCustomJs();
-    }
-
+window.onload = function() {
+  if (location.host === "detail.1688.com") {
+    injectCustomJs();
+  }
+  if (/admin.*?hibobi.com/.test(location.host)) {
+    injectCustomJs();
+  }
 };
